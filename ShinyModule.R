577755a7fec5aabedabaee39d5acfeb0a3dafa46
alexvmt <alexandermerdiantarko@gmail.com>
2023-05-23 22:24:57 +0200

    draft #4 matthias

--- ShinyModule.R
+++ ShinyModule.R
@@ -23,11 +23,9 @@ shinyModuleUserInterface <- function(id, label) {
     fluidRow(
 	column(3,
              selectInput(ns("dropdown_indi"), "Individium:",
-		  # TODO: make reactive, depending on given data set
                   choices = c("Choice 1", "Choice 2", "Choice 3")),
 	     selectInput(ns("dropdown_date"), "Date range:",
-                  # TODO: decide if we show the last days of the time series or from today()
-                 choices = list("last day" = 1, "last week" = 7, "last month" = 30, "last 6 months" = 180, "last year" = 365, "all time" = 9999), selected = c("last year" = 365))
+                 choices = list("last day" = 1, "last week" = 7, "last month" = 30, "last 6 months" = 180, "last year" = 365, "all time" = 99999), selected = c("last year" = 365))
             #      # TODO: decide if we add this. default should be daily.
             #      choices = c("daily", "hourly"), selected = "daily"),
 	    # TODO: remove outlier: unprobable distances > 100km/h per day
@@ -66,7 +64,10 @@ observe({
     if (is.null(data)) return()
     # TODO: find a better/more stable way than gsub for removing the X
     # Update the dropdown with the unique values from the 'id' column of the loaded data
-    updateSelectInput(session, "dropdown_indi", choices = c(gsub("^X", "", namesIndiv(data)), "all"), selected = "all")
+    keys <- c(namesIndiv(data), "all")
+    values <- c(idData(data)$individual.local.identifier, "all") 
+    key_value_list <- setNames(values,keys)	    
+    updateSelectInput(session, "dropdown_indi", choices = key_value_list, selected = c("all" = "all")) 
 })
 
 
@@ -76,10 +77,9 @@ rctv_processed_data <- reactive({
     # get dimensions and unique ids
     data_tmp <- as.data.frame(data)
 
-
-    # TODO: check if necessary to filter / which id column is the right one?
+    
     # rename columns
-    id_col <- "individual.local.identifier"    #"tag-local-identifier"
+    id_col <- "individual.local.identifier"  #"individual.id"       #"individual.local.identifier"   
     timestamp_col <- "timestamp"
     lon_col <- "location.long"
     lat_col <- "location.lat"
@@ -224,8 +224,9 @@ output$ts_plot <- renderPlotly({
     p <- plot_ly(data_to_plot, x = ~date, y = ~distance_meters, type = 'scatter', mode = 'lines', name = id) %>%
 #            add_trace(y = ~Series2, name = 'Series 2', mode = 'lines') %>%
 #            add_trace(y = ~Series3, name = 'Series 3', mode = 'lines') %>%
-            layout(title = paste0("Distance per individual ", id, " between ", start_date, " and ", end_date))
-            
+            #layout(title = paste0("Distance per individual ", id, " between ", start_date, " and ", end_date), showlegend=TRUE)
+            layout(showlegend=TRUE, legend = list(orientation = "h", xanchor = "center", x=0.5, y=1) )
+
     p # Return the plot
 })
 
@@ -237,31 +238,54 @@ output$ts_plot <- renderPlotly({
 ######################
 
 
-# TODO: cite repo if we intend to take this as is otherwise maybe use data frame
-# TODO: check how to print proper names in legend / same as timeseries
-# TODO: filter for only one individual
+# TODO: consider using https://movevis.org/ to create an interactive map
 #### make map as reactive object to be able to save it ####
 mapFinal <- reactive({
     mv <- mvObj()
-    if(input$dropdown_indi == "all"){
-		# do nothing
-	}else{
-		# filter individuum
-		#mv <- mv[[input$dropdown_indi]]
-	}
-    
-    cols <- colorFactor(gnuplot(), domain=namesIndiv(mv))
-    map1 <- leaflet(mv) %>% addTiles()
-    if(class(mv)=="MoveStack"){
-      mvL <- move::split(mv)
-      for(i in mvL){
-        map1 <- addPolylines(map1, lng = coordinates(i)[,1],lat = coordinates(i)[,2],color=~cols(namesIndiv(i)),weight=5, opacity=0.7 ,highlightOptions = highlightOptions(color = "red",opacity = 1,weight = 2, bringToFront = TRUE))
-      }
-    }else{map1 <- addPolylines(map1, lng = coordinates(mv)[,1],lat = coordinates(mv)[,2],color=~cols(namesIndiv(mv)),weight=5, opacity=0.7 ,highlightOptions = highlightOptions(color = "red",opacity = 1,weight = 2, bringToFront = TRUE))}
-    map1  %>% addLegend(position= "topright", pal=cols, values=namesIndiv(mv), labels=namesIndiv(mv) ,opacity = 0.7) %>%
-      addScaleBar(position="bottomleft",options=scaleBarOptions(maxWidth = 100, metric = TRUE, imperial = F, updateWhenIdle = TRUE))
-})
 
+    # store individual colors and names
+    cols       <- rainbow(length(namesIndiv(mv))) 
+    data_spl <- move::split(mv)
+    indi_names_org <- namesIndiv(mv)
+
+    # convert to data frame to filter
+    mvdf <- as.data.frame(mv)
+
+    # filter for date range and individuals
+    if(input$dropdown_indi == "all"){
+	# do nothing
+    }else{
+	mvdf <- mvdf[mvdf$individual.local.identifier==input$dropdown_indi,]
+    }
+    
+    # convert back to move for plotting
+    mv <- move(mvdf)
+
+    # get remaining individuals
+    indi_names <- namesIndiv(mv)
+    selected_id <- which(indi_names_org %in% indi_names)
+
+    # create map with lines for each animal, check if only one element is in the selected set
+    map <- leaflet() %>%  addTiles() 
+
+    if(length(indi_names)>1){
+	for (i in seq(along=indi_names)){
+ 	   map <- map %>%
+	     addPolylines(data = coordinates(data_spl[[i]]), color = cols[i], opacity=0.6,  group = indi_names[i], weight=2) %>%
+	     addCircles(data = data_spl[[i]], fillOpacity = 0.3, opacity = 0.5, color = cols[i], group = indi_names[i])
+ 	}
+    }else{
+	 map <- map %>%
+             addPolylines(data = coordinates(mv), color = cols[selected_id], opacity=0.6,  group = indi_names_org[selected_id], weight=2) %>%
+             addCircles(data = mv, fillOpacity = 0.3, opacity = 0.5, color = cols[selected_id], group = indi_names_org[selected_id])
+
+    }
+
+
+    map  <- map %>%  addLegend(position= "topright", colors=cols[selected_id], labels=indi_names_org[selected_id] ,opacity = 0.6)  
+    map
+
+})
 
 ### render map to be able to see it ####
 output$mymap <- renderLeaflet({
@@ -331,20 +355,6 @@ rctv_summary <- reactive({
     # render table
     datatable(rctv_summary())
   })
-
-
-#   # plot time series
-#   output$timeseries_plot <- renderPlot({
-#    # You need to replace this with your actual data
-#    df <- data.frame(time = Sys.time() + seq(-10, 0, by = 1),
-#                     value = rnorm(11))
-#
- #   ggplot(df, aes(time, value)) +
- #     geom_line() +
- #     labs(x = "Time", y = "Value", title = "Time Series") +
- #     theme_minimal()
- # })
-
 
   return(mvObj)
 }
