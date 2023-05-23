65d4ae3f5c4c042adabe60dfcbd2f6fa48c35ec0
alexvmt <alexandermerdiantarko@gmail.com>
2023-05-23 22:21:55 +0200

    draft #3 matthias

--- ShinyModule.R
+++ ShinyModule.R
@@ -22,14 +22,20 @@ shinyModuleUserInterface <- function(id, label) {
     tags$style(type = 'text/css', ".col-sm-9 {padding: 15px;}"),  #prevent graphs from overlapping
     fluidRow(
 	column(3,
-             selectInput("dropdown", "Individium:",
+             selectInput(ns("dropdown_indi"), "Individium:",
 		  # TODO: make reactive, depending on given data set
                   choices = c("Choice 1", "Choice 2", "Choice 3")),
-	     actionButton("toggle", "Toggle")
+	     selectInput(ns("dropdown_date"), "Date range:",
+                  # TODO: decide if we show the last days of the time series or from today()
+                 choices = list("last day" = 1, "last week" = 7, "last month" = 30, "last 6 months" = 180, "last year" = 365, "all time" = 9999), selected = c("last year" = 365))
+            #      # TODO: decide if we add this. default should be daily.
+            #      choices = c("daily", "hourly"), selected = "daily"),
+	    # TODO: remove outlier: unprobable distances > 100km/h per day
+	    # checkbox   
 	),
 	column(9,
-#		plotOutput(ns("timeseries_plot"))
-                #plotlyOutput(ns("ts_plot"))
+        #plotOutput(ns("timeseries_plot"))
+        #plotlyOutput(ns("ts_plot"))
 		DT::dataTableOutput(ns("table"))
 	)
     ),
@@ -41,164 +47,187 @@ shinyModuleUserInterface <- function(id, label) {
         column(5,
                 leafletOutput(ns("mymap"))
         ),
-	column(4,
-                #DT::dataTableOutput(ns("table"))
-	       plotlyOutput(ns("ts_plot"))
-	)
+        column(4,
+                    #DT::dataTableOutput(ns("table"))
+            plotlyOutput(ns("ts_plot"))
+        )
     )
-    #fluidRow(
-    #	downloadButton(ns('savePlot'), 'Save Plot')
-    #)
   )
 }
 
 shinyModule <- function(input, output, session, data) {
-  #### interactive object to read in .RData file  ####
-  mvObj <- reactive({ data })
 
- # get dimensions and unique ids
- data_tmp <- as.data.frame(data)
+#### interactive object to read in .RData file  ####
+mvObj <- reactive({ data })
 
 
-# TODO: check if necessary to filter / which id column is the right one?
-# rename columns
-id_col <- "individual.id"    #"tag-local-identifier"
-timestamp_col <- "timestamp"
-lon_col <- "location.long"
-lat_col <- "location.lat"
-names(data_tmp)[names(data_tmp) == id_col] <- "id"
-names(data_tmp)[names(data_tmp) == timestamp_col] <- "timestamp"
-names(data_tmp)[names(data_tmp) == lon_col] <- "lon"
-names(data_tmp)[names(data_tmp) == lat_col] <- "lat"
+observe({
+    # Wait until the data is loaded
+    if (is.null(data)) return()
+    # TODO: find a better/more stable way than gsub for removing the X
+    # Update the dropdown with the unique values from the 'id' column of the loaded data
+    updateSelectInput(session, "dropdown_indi", choices = c(gsub("^X", "", namesIndiv(data)), "all"), selected = "all")
+})
 
 
-# get individuals
- individuals <- unique(data_tmp$id)
+# data depends on user input
+rctv_processed_data <- reactive({
+
+    # get dimensions and unique ids
+    data_tmp <- as.data.frame(data)
 
 
- # create year and date columns
- data_tmp$date <- as.Date(format(data_tmp$timestamp, format = "%Y-%m-%d"))
- data_tmp$year <- as.integer(format(data_tmp$timestamp, format = "%Y"))
-
- # create empty dataframe to store processed individual data
- processed_data <- data.frame(matrix(ncol = length(colnames(data_tmp)[-length(colnames(data_tmp))]), nrow = 0))
- colnames(processed_data) <- colnames(data_tmp)[-length(colnames(data_tmp))]
-
-
-# set last n days
-last_n_days <- 365
-
-for(individual in individuals) {
-
-  # filter data based on individual
-  individual_data <- data_tmp[data_tmp$id == individual, ]
-
-  # TODO: commented for now since object contains all columns and therefore more NAs
-  #       maybe interpolate with 0s and mark in an additional row that this data was missing
-  #       could be show in a different color in the visualization then
-  # drop rows with missing values
-  #individual_data <- na.omit(individual_data)
-
-  # drop duplicated rows
-  individual_data <- individual_data[!duplicated(individual_data[c("id", "timestamp")]), ]
-
-  # extract max and min date
-  max_date <- max(individual_data$date)
-  min_date <- max_date - last_n_days
-
-  # filter data based on date range
-  individual_data <- individual_data[(individual_data$date >= min_date) & (individual_data$date <= max_date), ]
-
-  # append processed data to existing dataframe
-  processed_data <- rbind(processed_data, individual_data)
-
-}
+    # TODO: check if necessary to filter / which id column is the right one?
+    # rename columns
+    id_col <- "individual.local.identifier"    #"tag-local-identifier"
+    timestamp_col <- "timestamp"
+    lon_col <- "location.long"
+    lat_col <- "location.lat"
+    names(data_tmp)[names(data_tmp) == id_col] <- "id"
+    names(data_tmp)[names(data_tmp) == timestamp_col] <- "timestamp"
+    names(data_tmp)[names(data_tmp) == lon_col] <- "lon"
+    names(data_tmp)[names(data_tmp) == lat_col] <- "lat"
 
 
-# order data
-processed_data <- processed_data[order(processed_data$id, processed_data$timestamp), ]
-
-# create lag columns
-processed_data$id_lag <- c(NA, head(processed_data$id, -1))
-processed_data$lon_lag <- c(NA, head(processed_data$lon, -1))
-processed_data$lat_lag <- c(NA, head(processed_data$lat, -1))
-
-processed_data$id_lag <- ifelse(processed_data$id == processed_data$id_lag,
-                                processed_data$id_lag,
-                                NA)
-
-processed_data$lon_lag <- ifelse(processed_data$id == processed_data$id_lag,
-                                 processed_data$lon_lag,
-                                 NA)
-
-processed_data$lat_lag <- ifelse(processed_data$id == processed_data$id_lag,
-                                 processed_data$lat_lag,
-                                 NA)
-
-# calculate distance between two measurements
-calculate_distance_in_meters_between_coordinates <- function(lon_a, lat_a, lon_b, lat_b) {
-
-  if(anyNA(c(lon_a, lat_a, lon_b, lat_b))) return(NA)
-
-  distm(c(lon_a, lat_a), c(lon_b, lat_b), fun = distHaversine)
-
-}
+    # get individuals
+    individuals <- unique(data_tmp$id)
 
 
+    # create year and date columns
+    data_tmp$date <- as.Date(format(data_tmp$timestamp, format = "%Y-%m-%d"))
+    data_tmp$year <- as.integer(format(data_tmp$timestamp, format = "%Y"))
 
-processed_data$distance_meters <- mapply(lon_a = processed_data$lon,
-                                         lat_a = processed_data$lat,
-                                         lon_b = processed_data$lon_lag,
-                                         lat_b = processed_data$lat_lag,
-                                         FUN = calculate_distance_in_meters_between_coordinates)
+    # create empty dataframe to store processed individual data
+    processed_data <- data.frame(matrix(ncol = length(colnames(data_tmp)[-length(colnames(data_tmp))]), nrow = 0))
+    colnames(processed_data) <- colnames(data_tmp)[-length(colnames(data_tmp))]
 
-# aggregate distances by time interval and individual
-data_agg_id_date <- aggregate(distance_meters ~ date + id, data = processed_data, FUN = sum)
 
-# get number of aggregated observations per individual
-aggregate(cbind(count = id) ~ id, data = data_agg_id_date, FUN = function(x){NROW(x)})
+    # select time window
+    last_n_days <- as.numeric(input$dropdown_date)
 
-# TODO: remove the selection of a single individual
-# select individual
-id <- sample(individuals, 1)
 
-# plot timeseries for selected individual
-data_to_plot <- data_agg_id_date[data_agg_id_date$id == id, ]
-start_date <- min(data_to_plot$date)
-end_date <- max(data_to_plot$date)
+    for(individual in individuals) {
+        # filter data based on individual
+        individual_data <- data_tmp[data_tmp$id == individual, ]
 
-if (dim(data_to_plot)[1] > 30) {
-  scale <- "1 week"
-} else {
-  scale <- "1 day"
-}
+        # TODO: commented for now since object contains all columns and therefore more NAs
+        #       maybe interpolate with 0s and mark in an additional row that this data was missing
+        #       could be show in a different color in the visualization then
+        # drop rows with missing values
+        #individual_data <- na.omit(individual_data)
+
+        # drop duplicated rows
+        individual_data <- individual_data[!duplicated(individual_data[c("id", "timestamp")]), ]
+
+        # extract max and min date
+        max_date <- max(individual_data$date)
+        min_date <- max_date - last_n_days
+
+        # filter data based on date range
+        individual_data <- individual_data[(individual_data$date >= min_date) & (individual_data$date <= max_date), ]
+
+        # append processed data to existing dataframe
+        processed_data <- rbind(processed_data, individual_data)
+    }
+
+
+    # order data
+    processed_data <- processed_data[order(processed_data$id, processed_data$timestamp), ]
+
+    # create lag columns
+    processed_data$id_lag <- c(NA, head(processed_data$id, -1))
+    processed_data$lon_lag <- c(NA, head(processed_data$lon, -1))
+    processed_data$lat_lag <- c(NA, head(processed_data$lat, -1))
+
+    processed_data$id_lag <- ifelse(processed_data$id == processed_data$id_lag,
+                                    processed_data$id_lag,
+                                    NA)
+
+    processed_data$lon_lag <- ifelse(processed_data$id == processed_data$id_lag,
+                                    processed_data$lon_lag,
+                                    NA)
+
+    processed_data$lat_lag <- ifelse(processed_data$id == processed_data$id_lag,
+                                    processed_data$lat_lag,
+                                    NA)
+
+    # calculate distance between two measurements
+    calculate_distance_in_meters_between_coordinates <- function(lon_a, lat_a, lon_b, lat_b) {
+        if(anyNA(c(lon_a, lat_a, lon_b, lat_b))) return(NA)
+        distm(c(lon_a, lat_a), c(lon_b, lat_b), fun = distHaversine)
+    }
+
+
+
+    processed_data$distance_meters <- mapply(lon_a = processed_data$lon,
+                                            lat_a = processed_data$lat,
+                                            lon_b = processed_data$lon_lag,
+                                            lat_b = processed_data$lat_lag,
+                                            FUN = calculate_distance_in_meters_between_coordinates)
+
+
+    rctv_processed_data <- processed_data
+    rctv_processed_data
+
+}) # end of reactive data processing
+
+
+rctv_agg_data <- reactive({
+
+            # aggregate distances by time interval and individual
+            data_agg_id_date <- aggregate(distance_meters ~ date + id, data = rctv_processed_data(), FUN = sum)
+
+    data_agg_id_date
+
+})
 
 
 ########################
 # Time serie plot
 ########################
 
+## plot all time series
+#output$timeseries_plot <- renderPlot({	
+#  ggplot(data_to_plot, aes(x = date, y = distance_meters, group = 1)) +
+#  geom_line(linewidth = 0.75) +
+#  scale_x_date(breaks = seq(start_date, end_date, by = scale)) +
+#  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
+#  ggtitle(paste0("Distance in meters moved per day for individual ", id, " between ", start_date, " and ", end_date))
+#})
 
-# TODO: plot all time series
-output$timeseries_plot <- renderPlot({
-ggplot(data_to_plot, aes(x = date, y = distance_meters, group = 1)) +
-  geom_line(linewidth = 0.75) +
-  scale_x_date(breaks = seq(start_date, end_date, by = scale)) +
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
-  ggtitle(paste0("Distance in meters moved per day for individual ", id, " between ", start_date, " and ", end_date))
-})
+
+# Render the Plotly plot
+output$ts_plot <- renderPlotly({
+
+    # load reactive data
+    data_agg_id_date <- rctv_agg_data()
+
+    if(input$dropdown_indi=="all"){
+        # TODO: define a function to plot all and not just the first given one
+        id <- data_agg_id_date$id[1]
+    }else{
+        id <- input$dropdown_indi
+    }
 
 
-# TODO: plot all time series
-    # Render the Plotly plot
-    output$ts_plot <- renderPlotly({
-        p <- plot_ly(data_to_plot, x = ~date, y = ~distance_meters, type = 'scatter', mode = 'lines', name = id) %>%
+    # plot timeseries for selected individual
+    data_to_plot <- data_agg_id_date[data_agg_id_date$id == id, ]
+    start_date <- min(data_to_plot$date)
+    end_date <- max(data_to_plot$date)
+
+    if (dim(data_to_plot)[1] > 30) {
+    scale <- "1 week"
+    } else {
+    scale <- "1 day"
+    }
+
+    p <- plot_ly(data_to_plot, x = ~date, y = ~distance_meters, type = 'scatter', mode = 'lines', name = id) %>%
 #            add_trace(y = ~Series2, name = 'Series 2', mode = 'lines') %>%
 #            add_trace(y = ~Series3, name = 'Series 3', mode = 'lines') %>%
-            layout(title = paste0("Distance in meters moved per day for individual ", id, " between ", start_date, " and ", end_date))
+            layout(title = paste0("Distance per individual ", id, " between ", start_date, " and ", end_date))
             
-        p # Return the plot
-    })
+    p # Return the plot
+})
 
 
 
@@ -208,11 +237,19 @@ ggplot(data_to_plot, aes(x = date, y = distance_meters, group = 1)) +
 ######################
 
 
-
+# TODO: cite repo if we intend to take this as is otherwise maybe use data frame
 # TODO: check how to print proper names in legend / same as timeseries
-  #### make map as reactive object to be able to save it ####
-  mapFinal <- reactive({
+# TODO: filter for only one individual
+#### make map as reactive object to be able to save it ####
+mapFinal <- reactive({
     mv <- mvObj()
+    if(input$dropdown_indi == "all"){
+		# do nothing
+	}else{
+		# filter individuum
+		#mv <- mv[[input$dropdown_indi]]
+	}
+    
     cols <- colorFactor(gnuplot(), domain=namesIndiv(mv))
     map1 <- leaflet(mv) %>% addTiles()
     if(class(mv)=="MoveStack"){
@@ -223,13 +260,13 @@ ggplot(data_to_plot, aes(x = date, y = distance_meters, group = 1)) +
     }else{map1 <- addPolylines(map1, lng = coordinates(mv)[,1],lat = coordinates(mv)[,2],color=~cols(namesIndiv(mv)),weight=5, opacity=0.7 ,highlightOptions = highlightOptions(color = "red",opacity = 1,weight = 2, bringToFront = TRUE))}
     map1  %>% addLegend(position= "topright", pal=cols, values=namesIndiv(mv), labels=namesIndiv(mv) ,opacity = 0.7) %>%
       addScaleBar(position="bottomleft",options=scaleBarOptions(maxWidth = 100, metric = TRUE, imperial = F, updateWhenIdle = TRUE))
-  })
+})
 
 
-  ### render map to be able to see it ####
-  output$mymap <- renderLeaflet({
+### render map to be able to see it ####
+output$mymap <- renderLeaflet({
     mapFinal()
-  })
+})
 
 
 
@@ -237,54 +274,62 @@ ggplot(data_to_plot, aes(x = date, y = distance_meters, group = 1)) +
 # Statistics table
 ########################
 
+rctv_summary <- reactive({
 
-# create empty dataframe to store summary data
-summary <- data.frame(matrix(ncol = 5, nrow = 0))
-colnames(summary) <- c("individual", "last_n_days", "number_observations", "total_distance", "mean_distance")
+    data_agg_id_date <- rctv_agg_data()
 
-# compute summary statistics for last n days per individual
-for(individual in individuals) {
+    # get individuals
+    individuals <- unique(data_agg_id_date$id)
 
-  for (last_n_days in c(3, 7, 14, 21, 28)) {
+    # TODO: add column for n days with almost no movement/2x below stdev
+    # TODO: missing data today / in time series?
+    # TODO: less movement than expected => below_today, to be discussed
+    # create empty dataframe to store summary data
+    colnames_summary <- c("individual", "#observations", "#days w/o observations", "today below avg.", "total distance", "avg. distance")
+    summary <- data.frame(matrix(ncol = length(colnames_summary), nrow = 0))
+    colnames(summary) <- colnames_summary
 
-  # filter data based on individual
-  individual_aggregated_data <- data_agg_id_date[data_agg_id_date$id == individual, ]
+    # compute summary statistics for last n days per individual
+    for(individual in individuals) {
 
-  # get max date
-  max_date <- max(individual_aggregated_data$date)
+        # filter data based on individual
+        individual_agg_data <- data_agg_id_date[data_agg_id_date$id == individual, ]
 
-  # filter data based on date
-  individual_aggregated_data_filtered <- individual_aggregated_data[individual_aggregated_data$date > max_date - last_n_days, ]
+        # calculate missing days / account for all set to 99999
+        missing_days <- as.numeric(input$dropdown_date) - dim(individual_agg_data)[1]
+        missing_days <- ifelse(missing_days < 0, 0, missing_days)
 
-  # create empty dataframe to store individual summary data
-  individual_summary_data <- data.frame(matrix(ncol = 5, nrow = 0))
-  colnames(individual_summary_data) <- c("individual", "last_n_days", "number_observations", "total_distance", "mean_distance")
-  individual_summary_data[1, 1] = individual
-  individual_summary_data[1, 2] = last_n_days
+        # calculate below average of today
+        avg_distance <- mean(individual_agg_data$distance_meters)
+        sd_distance  <- sd(individual_agg_data$distance_meters)
+        max_date     <- max(individual_agg_data$date)
+        meters_today <- individual_agg_data[individual_agg_data$date== max_date, "distance_meters"] 
+        below_today  <- ifelse(meters_today < avg_distance - (1.5 * sd_distance), "yes", "no")
 
-  # compute summary statistics
-  individual_summary_data[1, 3] = dim(individual_aggregated_data_filtered)[1]
-  individual_summary_data[1, 4] = sum(individual_aggregated_data_filtered$distance_meters)
-  individual_summary_data[1, 5] = mean(individual_aggregated_data_filtered$distance_meters)
+        # store values
+        individual_summary_data <- c(unique(individual_agg_data$id),
+                        dim(individual_agg_data)[1],
+                        missing_days,
+                        below_today,
+                        round(sum(individual_agg_data$distance_meters), 2),
+                        round(avg_distance, 2)
+                        )
 
-  # append summary data to existing dataframe
-  summary <- rbind(summary, individual_summary_data)
+        # append summary data to existing dataframe
+        summary[nrow(summary)+1, ] <- individual_summary_data
 
-  }
+    }
+    
 
-}
-   
+    summary
 
-
+})
 
 
   # plot a table
   output$table <- DT::renderDataTable({
-    # You need to replace this with your actual data
-    df <- data.frame(Category = c("Stat 1", "Stat 2", "Stat 3"),
-                     Value = c(123, 456, 789))
-
-    datatable(summary)
+    # render table
+    datatable(rctv_summary())
   })
 
 
